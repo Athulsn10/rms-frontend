9f6c082c20125b28ceff8658c27e373c0840437a
Athulsn10 <athulnair10@outlook.com>
2024-11-17 19:15:21 +0530

    qr button

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import './App.css'
 import Home from './app/pages/home/home';
 import Navbar from "@/components/nav/navbar";
 import Search from "./app/pages/search/search";
+import Qr from "./app/pages/qr/qr";
 import { Route, Routes, useLocation  } from 'react-router-dom';
 import Authentication from './app/auth/authentication';
 
 const App =()=> {
   const location = useLocation();
-  const hideNavbarPaths = ["/auth"];
+  const hideNavbarPaths = ["/auth","/qrscanner"];
   const showNavbar = !hideNavbarPaths.includes(location.pathname);
   return (
     <>
@@ -16,6 +17,7 @@ const App =()=> {
       <Route path='/auth' element={<Authentication/>}/>
       <Route path='/' element={<Home/>}/>
       <Route path='/Search' element={<Search/>}/>
+      <Route path='/qrscanner' element={<Qr/>}/>
      </Routes>
     </>
   )
