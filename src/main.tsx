8f44f1439fdf23126fa112618dcec35faeae7406
Athulsn10 <athulnair10@outlook.com>
2024-11-23 21:32:14 +0530

    removed bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import './index.css'
 import React from 'react'
 import App from './App.tsx'
 import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import MyProvider from './app/context/provider.tsx'
 
