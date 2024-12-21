2944779b75b7fb6b433ce347ff00fc12a7acb6b1
Athulsn10 <athulnair10@outlook.com>
2024-12-21 16:41:19 +0530

    replaced checkbox with multiselect

--- src/app/pages/customer/profile/profileContent.tsx
+++ src/app/pages/customer/profile/profileContent.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import toast, { Toaster } from 'react-hot-toast';
-import { Checkbox } from "@/components/ui/checkbox";
+import { MultiSelect } from "@/components/ui/multiselect";
 import { getUserDetail, updateUserProfile } from './profileService';
 import { User, MapPin, Building, Home, NutOff, Phone, Cake, Loader2, CircleAlert, CircleCheck, ArrowRight, ArrowLeft, Check } from 'lucide-react';
 
@@ -27,6 +27,7 @@ interface FormData {
 const ProfileContent = () => {
     const [currentStep, setCurrentStep] = useState(0);
     const [isLoading, setIsLoading] = useState(false);
+    const [fetchingData, setFetchingData] = useState(false);
     const [formData, setFormData] = useState<FormData>({
         name: '',
         phone: '',
@@ -42,23 +43,58 @@ const ProfileContent = () => {
         allergies: [],
     });
     const [errors, setErrors] = useState<Record<string, string>>({});
-
+    const [selectedAllergies, setSelectedAllergies] = useState<string[]>([]);
     const commonAllergies = [
-        "Peanuts",
-        "Tree Nuts",
-        "Milk",
-        "Egg",
-        "Wheat",
-        "Soy",
-        "Fish",
-        "Shellfish",
-        "Sesame",
-        "Mustard",
-        "Celery",
-        "Lupin",
-        "Sulphites",
-        "Gluten",
-        "Lactose",
+        { value: "peanuts", label: "Peanuts" },
+        { value: "shellfish", label: "Shellfish" },
+        { value: "milk", label: "Milk" },
+        { value: "eggs", label: "Eggs" },
+        { value: "tree_nuts", label: "Tree Nuts" },
+        { value: "soy", label: "Soy" },
+        { value: "wheat", label: "Wheat" },
+        { value: "fish", label: "Fish" },
+        { value: "sesame", label: "Sesame" },
+        { value: "dust_mites", label: "Dust Mites" },
+        { value: "pollen", label: "Pollen" },
+        { value: "mold", label: "Mold" },
+        { value: "animal_dander", label: "Animal Dander" },
+        { value: "latex", label: "Latex" },
+        { value: "insect_stings", label: "Insect Stings" },
+        { value: "penicillin", label: "Penicillin" },
+        { value: "aspirin", label: "Aspirin" },
+        { value: "sulfites", label: "Sulfites" },
+        { value: "red_dye", label: "Red Dye" },
+        { value: "perfumes", label: "Perfumes" },
+        { value: "nickel", label: "Nickel" },
+        { value: "chocolate", label: "Chocolate" },
+        { value: "gluten", label: "Gluten" },
+        { value: "corn", label: "Corn" },
+        { value: "citrus_fruits", label: "Citrus Fruits" },
+        { value: "strawberries", label: "Strawberries" },
+        { value: "bananas", label: "Bananas" },
+        { value: "kiwi", label: "Kiwi" },
+        { value: "avocado", label: "Avocado" },
+        { value: "sunflower_seeds", label: "Sunflower Seeds" },
+        { value: "pork", label: "Pork" },
+        { value: "beef", label: "Beef" },
+        { value: "chicken", label: "Chicken" },
+        { value: "celery", label: "Celery" },
+        { value: "mustard", label: "Mustard" },
+        { value: "garlic", label: "Garlic" },
+        { value: "onion", label: "Onion" },
+        { value: "pepper", label: "Pepper" },
+        { value: "chili", label: "Chili" },
+        { value: "yeast", label: "Yeast" },
+        { value: "tomatoes", label: "Tomatoes" },
+        { value: "spinach", label: "Spinach" },
+        { value: "peaches", label: "Peaches" },
+        { value: "apples", label: "Apples" },
+        { value: "grapes", label: "Grapes" },
+        { value: "melons", label: "Melons" },
+        { value: "caffeine", label: "Caffeine" },
+        { value: "alcohol", label: "Alcohol" },
+        { value: "preservatives", label: "Preservatives" },
+        { value: "food_colorings", label: "Food Colorings" },
     ];
 
     const formFields = [
@@ -187,19 +223,6 @@ const ProfileContent = () => {
         }
     };
 
-    const handleAllergyToggle = (allergy: string) => {
-        setFormData(prev => {
-            const updatedAllergies = prev.allergies.includes(allergy)
-                ? prev.allergies.filter(a => a !== allergy)
-                : [...prev.allergies, allergy];
-
-            return {
-                ...prev,
-                allergies: updatedAllergies
-            };
-        });
-    };
-
     const validateStep = (step: number) => {
         const currentFields = formFields[step];
         const newErrors = {} as Record<string, string>;
@@ -238,6 +261,7 @@ const ProfileContent = () => {
         e.preventDefault();
         setIsLoading(true);
         if (validateStep(currentStep)) {
+            formData.allergies = selectedAllergies;
             console.log('Form submitted:', formData);
             const response = await updateUserProfile(formData);
             if (response) {
@@ -257,33 +281,8 @@ const ProfileContent = () => {
 
     const currentFields = formFields[currentStep];
 
-    const renderAllergiesStep = () => (
-        <div className="space-y-4">
-            <div className="flex items-center space-x-2">
-                <NutOff className="h-5 w-5 text-gray-500" />
-                <p className="text-lg font-medium">Select Your Allergies</p>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-                {commonAllergies.map((allergy) => (
-                    <div key={allergy} className="flex items-center space-x-2">
-                        <Checkbox
-                            id={allergy}
-                            checked={formData.allergies.includes(allergy)}
-                            onCheckedChange={() => handleAllergyToggle(allergy)}
-                        />
-                        <Label
-                            htmlFor={allergy}
-                            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                        >
-                            {allergy}
-                        </Label>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-
     const getCurrentUser = async () => {
+        setFetchingData(true);
         const response = await getUserDetail();
         setFormData({
             name: response.name || '',
@@ -299,90 +298,112 @@ const ProfileContent = () => {
             },
             allergies: response.allergies || [],
         });
-    }
+        setSelectedAllergies(response.allergies || []);
+        setFetchingData(false);
+    };
 
     useEffect(() => {
         getCurrentUser()
     }, [])
 
     return (
-        <div className="flex w-full hover:shadow-md">
-            <div className="w-full">
-                <div className='flex justify-between me-5 items-center'>
-                    <div className="space-y-1 sm:p-8">
-                        <div className="text-2xl sm:text-3xl font-bold tracking-tight">
-                            Edit your Account
-                            <p className="text-sm mt-2">{`[ Page ${currentStep + 1} of ${formFields.length} ]`}</p>
-                        </div>
-                    </div>
-                    <div className="flex justify-end gap-3 sm:gap-4 mt-8 border-t pt-6">
-                        {currentStep > 0 && (
-                            <div onClick={handlePrevious} className="bg-orange-100 hover:bg-orange-200 p-2 rounded-full cursor-pointer">
-                                <ArrowLeft className="w-7 h-7"/>
+        <>
+            {fetchingData ? (
+                <div className='flex items-center justify-center h-96 text-orange-600'>
+                    <Loader2 className="w-9 h-9 animate-spin" />
+                </div>) :
+                (<div className="flex w-full">
+                    <div className="w-full">
+                        <div className='flex justify-between me-5 items-center'>
+                            <div className="space-y-1 sm:p-8">
+                                <div className="text-2xl sm:text-3xl font-bold tracking-tight">
+                                    Edit your Account
+                                    <p className="text-sm mt-2">{`[ Page ${currentStep + 1} of ${formFields.length} ]`}</p>
+                                </div>
                             </div>
-                        )}
-                        {currentStep < formFields.length - 1 ? (
-                            <div onClick={handleNext} className="bg-orange-100 hover:bg-orange-200 p-2 rounded-full cursor-pointer">
-                                <ArrowRight className="w-7 h-7"/>
-                            </div>
-                        ) : (
-                            <div onClick={handleSubmit}className="">
-                                {
-                                    isLoading ? (
-                                        <div className="bg-orange-100 hover:bg-orange-200 p-2 rounded-full cursor-progress">
-                                            <Loader2 className="w-7 h-7 animate-spin"/>
-                                        </div>
-                                    ) : (
-                                       <div className="bg-orange-100 hover:bg-orange-200 p-2 rounded-full cursor-pointer">
-                                            <Check className="w-7 h-7"/>
-                                       </div>
-                                    )
-                                }
-                            </div>
-                        )}
-                    </div>
-                </div>
-                <div className="p-6 sm:p-8">
-                    <form className="space-y-6">
-                        <div className={`${currentFields.some(field => field.id === "allergies") ? "" : "grid grid-cols-1 md:grid-cols-2"} gap-2 gap-x-6`}>
-                            {currentFields.map((field, index) => {
-                                
-                                if (field.id === "allergies") {
-                                    return renderAllergiesStep();
-                                }
-
-                                return (
-                                    <div key={index} className="space-y-2">
-                                        <Label htmlFor={field.id} className="text-sm font-medium block">
-                                            {field.label}
-                                        </Label>
-                                        <div className="relative">
-                                            <field.icon className="absolute left-3 top-8 -translate-y-1/2 h-5 w-5 text-gray-500" />
-                                            <Input
-                                                id={field.id}
-                                                type={field.type}
-                                                placeholder={field.placeholder}
-                                                className="px-10 md:py-8 h-12 rounded-none w-full bg-white"
-                                                value={(field.id.includes('.') ? formData.address[field.id.split('.')[1] as keyof AddressData] : formData[field.id as keyof FormData]) as any}
-                                                onChange={(e) => handleInputChange(field.id, e.target.value)}
-                                            />
-                                            <div style={{ height: '6px', marginTop: '2px', display: 'flex', justifyContent: 'end', width: '100%' }}>
-                                                {errors[field.id] && (
-                                                    <p className="text-red-500 text-xs">
-                                                        {errors[field.id]}
-                                                    </p>
-                                                )}
-                                            </div>
-                                        </div>
+                            <div className="flex justify-end gap-3 sm:gap-4 mt-8 border-t pt-6">
+                                {currentStep > 0 && (
+                                    <div onClick={handlePrevious} className="bg-orange-100 hover:bg-orange-200 p-2 rounded-full cursor-pointer">
+                                        <ArrowLeft className="w-7 h-7" />
                                     </div>
-                                );
-                            })}
+                                )}
+                                {currentStep < formFields.length - 1 ? (
+                                    <div onClick={handleNext} className="bg-orange-100 hover:bg-orange-200 p-2 rounded-full cursor-pointer">
+                                        <ArrowRight className="w-7 h-7" />
+                                    </div>
+                                ) : (
+                                    <div onClick={handleSubmit} className="">
+                                        {
+                                            isLoading ? (
+                                                <div className="bg-orange-100 hover:bg-orange-200 p-2 rounded-full cursor-progress">
+                                                    <Loader2 className="w-7 h-7 animate-spin" />
+                                                </div>
+                                            ) : (
+                                                <div className="bg-orange-100 hover:bg-orange-200 p-2 rounded-full cursor-pointer">
+                                                    <Check className="w-7 h-7" />
+                                                </div>
+                                            )
+                                        }
+                                    </div>
+                                )}
+                            </div>
                         </div>
-                    </form>
-                </div>
-            </div>
-            <Toaster />
-        </div>
+                        <div className="p-6 sm:p-8">
+                            <form className="space-y-6">
+                                <div className={`${currentFields.some(field => field.id === "allergies") ? "" : "grid grid-cols-1 md:grid-cols-2"} gap-x-8 md:gap-y-3`}>
+                                    {currentFields.map((field, index) => {
+
+                                        if (field.id === "allergies") {
+                                            return (
+                                                <div>
+                                                    <p className="flex items-center justify-center text-xl font-bold pb-5">{field.label}</p>
+                                                    <MultiSelect
+                                                        className="bg-white border-r-0 py-5 hover:bg-white"
+                                                        options={commonAllergies}
+                                                        defaultValue={selectedAllergies}
+                                                        onValueChange={setSelectedAllergies}
+                                                        placeholder="Modify your allergies"
+                                                        variant="rmscolor"
+                                                        animation={2}
+                                                        maxCount={6} />
+                                                </div>
+                                            )
+                                        }
+
+                                        return (
+                                            <div key={index} className="space-y-2">
+                                                <Label htmlFor={field.id} className="text-sm font-medium block">
+                                                    {field.label}
+                                                </Label>
+                                                <div className="relative">
+                                                    <field.icon className="absolute left-3 top-8 -translate-y-1/2 h-5 w-5 text-gray-500" />
+                                                    <Input
+                                                        id={field.id}
+                                                        type={field.type}
+                                                        placeholder={field.placeholder}
+                                                        className="px-10 md:py-8 h-12 rounded-none w-full bg-white"
+                                                        value={(field.id.includes('.') ? formData.address[field.id.split('.')[1] as keyof AddressData] : formData[field.id as keyof FormData]) as any}
+                                                        onChange={(e) => handleInputChange(field.id, e.target.value)}
+                                                    />
+                                                    <div style={{ height: '6px', marginTop: '2px', display: 'flex', justifyContent: 'end', width: '100%' }}>
+                                                        {errors[field.id] && (
+                                                            <p className="text-red-500 text-xs">
+                                                                {errors[field.id]}
+                                                            </p>
+                                                        )}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <Toaster />
+                </div>)
+            }
+        </>
     );
 };
 
