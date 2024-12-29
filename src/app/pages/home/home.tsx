1111faf1565aa4068e75feaa40944b13e6c5585f
Athulsn10 <athulnair10@outlook.com>
2024-12-29 12:30:08 +0530

    ui

--- src/app/pages/home/home.tsx
+++ src/app/pages/home/home.tsx
@@ -13,19 +13,26 @@ interface Restaurant {
 function home() {
   return (
     <>
-      <p className="mt-10 md:mx-28 ms-6 pb-5 font-bold md:text-xl">Top restaurant chains in "LOCATION"</p>
-      <div className='flex w-[full] h-fit overflow-x-scroll custom-scroll-bar md:mx-24 md:py-5'>
-        {restaurant.map((restaurant: Restaurant) => (
-         <div key={restaurant.id}>
-            <RestaurantCard
-              title={restaurant.title}
-              rating={restaurant.rating}
-              category={restaurant.category}
-              area={restaurant.area}
-              imgUrl={restaurant.imgUrl}
-            />
-         </div>
-        ))}
+      <div className='md:mx-24 md:py-5 mx-4 my-4 h-fit'>
+        <div className='h-[400px] w-full bg-orange-100 rounded-lg '>
+
+        </div>
+      </div>
+      <div>
+        <p className="md:mx-28 ms-6 pb-5 font-bold md:text-xl">Top restaurant chains in "LOCATION"</p>
+        <div className='flex w-[full] h-fit overflow-x-scroll custom-scroll-bar md:mx-24 md:py-5'>
+          {restaurant.map((restaurant: Restaurant) => (
+            <div key={restaurant.id}>
+              <RestaurantCard
+                title={restaurant.title}
+                rating={restaurant.rating}
+                category={restaurant.category}
+                area={restaurant.area}
+                imgUrl={restaurant.imgUrl}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </>
   )
