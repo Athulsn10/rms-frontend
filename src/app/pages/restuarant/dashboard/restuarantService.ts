f7af56e37e03023dde573d62548239f42788ba14
Athulsn10 <athulnair10@outlook.com>
2025-01-05 14:41:09 +0530

    add edit, delete, and image upload functionalities to menu service

--- src/app/pages/restuarant/dashboard/restuarantService.ts
+++ src/app/pages/restuarant/dashboard/restuarantService.ts
@@ -32,18 +32,60 @@ export const createMenu = async (formData: any) => {
                 ...(token && { 'Authorization': `Bearer ${token}` })
             },
         });
-        console.log('response.data.statusCode:',response.status)
         return response.status >= 200 && response.status <= 300 ? true : false;
     } catch (error: any) {
         return error.response?.data?.message
     }
 };
 
+export const editMenu = async (id: string, formData: any) => {
+    try {
+        const url = import.meta.env.VITE_BASE_URL;
+        const token = localStorage.getItem('token');
+        const response:any = await axios.patch(`${url}menus/${id}`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                ...(token && { 'Authorization': `Bearer ${token}` })
+            },
+        });
+        return response.status >= 200 && response.status <= 300 ? true : false;
+    } catch (error: any) {
+        return error.response?.data?.message
+    }
+};
 
 export const getMenus = async () => {
     try {
         const response = await http.get(`/menus`);
         return response.data.data;
+    } catch (error: any) {
+        return error.response?.data?.message
+    }
+};
+
+export const deleteMenu = async (itemId: String) => {
+    try {
+        const response = await http.delete(`/menus/${itemId}`);
+        console.log('response:',response)
+        return response.status >= 200 && response.status <= 300 ? true : false;
+    } catch (error: any) {
+        return error.response?.data?.message
+    }
+};
+
+export const imageUpload = async (file: any) => {
+    try {
+        const url = import.meta.env.VITE_BASE_URL;
+        const token = localStorage.getItem('token');
+        const formData = new FormData();
+        formData.append('image', file);
+        const response:any = await axios.post(`${url}menus/get-ingredients`, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                ...(token && { 'Authorization': `Bearer ${token}` })
+            },
+        });
+        return response.status >= 200 && response.status <= 300 ? response.data.data.ingredients : false;
     } catch (error: any) {
         return error.response?.data?.message
     }
