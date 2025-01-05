00ac9514775b691c48ab9431baada7493bb744d0
Athulsn10 <athulnair10@outlook.com>
2025-01-05 14:43:13 +0530

    feat: implement restaurant fetching by city and update UI to display location

--- src/app/pages/home/home.tsx
+++ src/app/pages/home/home.tsx
@@ -1,34 +1,59 @@
+import { useEffect, useState } from 'react';
 import RestaurantCard from '@/components/restuarantCard/restuarantCard'
-import restaurant from './mockCard.js';
+import { getRestuarents, getRestuarentsByCity } from './homeService.js';
 
 interface Restaurant {
   id: number,
-  title: string,
+  name: string,
   rating: string,
-  category: string,
+  tableCount: string,
   area: string,
   imgUrl: string
 }
 
 function home() {
+  const [location, setLocation] = useState('')
+  const [restaurantList, setRestaurantList] = useState([]);
+
+  const fetchRestuarants = async () => {
+    const city = localStorage.getItem('city');
+    if (city) {
+      setLocation(city);
+      const response = await getRestuarents();
+      if (response) {
+        setRestaurantList(response);
+        console.log(response)
+      }
+    }
+  }
+
+  useEffect(() => {
+    fetchRestuarants();
+  }, []);
+
   return (
     <>
       <div className='md:mx-24 md:py-5 mx-4 my-4 h-fit'>
-        <div className='h-[400px] w-full bg-orange-100 rounded-lg '>
-
+        <div className='h-auto w-full bg-white rounded-xl'>
+          <img
+            src="/banner.jpg"
+            alt=""
+            className="w-full h-full object-cover"
+          />
         </div>
       </div>
+
       <div>
-        <p className="md:mx-28 ms-6 pb-5 font-bold md:text-xl">Top restaurant chains in "LOCATION"</p>
+        <p className="md:mx-28 ms-6 pb-5 font-bold md:text-xl">Top restaurant chains in {location}</p>
         <div className='flex w-[full] h-fit overflow-x-scroll custom-scroll-bar md:mx-24 md:py-5'>
-          {restaurant.map((restaurant: Restaurant) => (
+          {restaurantList.map((restaurant: Restaurant) => (
             <div key={restaurant.id}>
               <RestaurantCard
-                title={restaurant.title}
+                title={restaurant.name}
                 rating={restaurant.rating}
-                category={restaurant.category}
+                category={restaurant.tableCount}
                 area={restaurant.area}
-                imgUrl={restaurant.imgUrl}
+                imgUrl='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/86f52324ecee5fc94cbf63c4a568b672'
               />
             </div>
           ))}
