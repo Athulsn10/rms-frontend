d53b50bd2ed10698f5e08225719bc1210076af26
Athulsn10 <athulnair10@outlook.com>
2025-01-25 11:47:07 +0530

    feat: add loading indicator for orders fetching in dashboard

--- src/app/pages/restuarant/dashboard/orders.tsx
+++ src/app/pages/restuarant/dashboard/orders.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useState } from 'react';
 import OrderModal from "./orderModal";
 import { Button } from "@/components/ui/button";
-import { CircleAlert, CircleCheck, Clock, Edit2, Eye } from 'lucide-react';
+import { CircleAlert, CircleCheck, Clock, Edit2, Eye, Loader2 } from 'lucide-react';
 import toast, { Toaster } from 'react-hot-toast';
 import { getAllOrders, orderPaymentStatus, updateOrder } from "./restuarantService";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
@@ -33,10 +33,11 @@ function Orders() {
   };
 
   const fetchOrders = async () => {
+    setIsLoading(true);
     const response = await getAllOrders();
     if (response) {
       setOrder(response.reverse());
-      console.log('response:',response)
+      setIsLoading(false);
     }
   };
 
@@ -94,104 +95,112 @@ function Orders() {
   }, [])
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-      {orders.map((order:any) => (
-        <Card key={order._id} className="w-full border-none">
-          <CardContent className="pt-6">
-            <div className="space-y-4">
-              <div className="flex justify-between items-start">
-                <div>
-                  <div className="flex items-center space-x-2">
-                    <span className="text-lg font-medium">Table {order.tableNumber}</span>
-                    <span className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${statusColors[order.status]}`}>
-                      {order.status}
-                    </span>
-                    <span className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${order.isPaid ? "bg-green-100 text-green-800" : "bg-red-100 text-red-800"} `}>
-                      {order.isPaid ? "Paid" : "Not Paid"}
-                    </span>
-                  </div>
-                  <div className="flex items-center space-x-1 text-sm text-gray-500 mt-1">
-                    <Clock className="h-4 w-4" />
-                    <span>{formatDate(order.createdAt)}</span>
+    <>
+     {isLoading ? (
+      <div className='flex items-center justify-center h-full text-orange-600'>
+       <Loader2 className="w-9 h-9 animate-spin" />
+      </div>
+     ):(
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+        {orders.map((order:any) => (
+          <Card key={order._id} className="w-full border-none">
+            <CardContent className="pt-6">
+              <div className="space-y-4">
+                <div className="flex justify-between items-start">
+                  <div>
+                    <div className="flex items-center space-x-2">
+                      <span className="text-lg font-medium">Table {order.tableNumber}</span>
+                      <span className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${statusColors[order.status]}`}>
+                        {order.status}
+                      </span>
+                      <span className={`inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium ${order.isPaid ? "bg-green-100 text-green-800" : "bg-red-100 text-red-800"} `}>
+                        {order.isPaid ? "Paid" : "Not Paid"}
+                      </span>
+                    </div>
+                    <div className="flex items-center space-x-1 text-sm text-gray-500 mt-1">
+                      <Clock className="h-4 w-4" />
+                      <span>{formatDate(order.createdAt)}</span>
+                    </div>
                   </div>
                 </div>
-              </div>
-
-              <div className="space-y-2">
-                <div className="flex space-x-5">
-                  {/* view */}
-                  <Dialog >
-                    <DialogTrigger asChild>
-                      <Button disabled={order.status === 'Cancelled'} className="hover:bg-orange-100 rounded-none bg-transparent border-orange-400 hover:border-swiggyOrange border-2 text-swiggyOrange disabled:pointer-events-none">
-                        <Eye className="h-4 w-4" />View Order
-                      </Button>
-                    </DialogTrigger>
-                    <DialogContent>
-                      <DialogHeader>
-                        <DialogTitle>Order Details</DialogTitle>
-                      </DialogHeader>
-                      <OrderModal order={order} />
-                    </DialogContent>
-                  </Dialog>
-                  {/* Edit */}
-                  <Dialog open={dialogOpen === order._id} onOpenChange={(open) => {handleDialogChange(open, order)}}>
-                    <DialogTrigger asChild>
-                      <Button disabled={order.status === 'Cancelled'} className="flex-1 gap-2 rounded-none bg-orange-600 hover:bg-orange-700 disabled:pointer-events-none">
-                        <Edit2 className="w-4 h-4"/> Edit Status
-                      </Button>
-                    </DialogTrigger>
-                    <DialogContent>
-                      <DialogHeader>
-                        <DialogTitle>Update Order Status</DialogTitle>
-                      </DialogHeader>
-                      <div className="py-4">
-                        <div className="grid grid-cols-1 md:grid-cols-2">
-                          <Select value={order.status} onValueChange={(value) => handleStatusUpdate(order, value)}>
-                            <SelectTrigger className="w-full">
-                              <SelectValue placeholder="Select status" />
-                            </SelectTrigger>
-                            <SelectContent>
-                              <SelectItem value="Ordered">Ordered</SelectItem>
-                              <SelectItem value="Preparing">Preparing</SelectItem>
-                              {/* <SelectItem value="Completed">Completed</SelectItem> */}
-                              <SelectItem value="Delivered">Delivered</SelectItem>
-                              <SelectItem value="Cancelled">Cancelled</SelectItem>
-                            </SelectContent>
-                          </Select>
-                          <Select value={order.isPaid.toString()} onValueChange={(value) => handlePaymentStatus(order, value === "true")}>
-                            <SelectTrigger className="w-full">
-                              <SelectValue placeholder="Select status" />
-                            </SelectTrigger>
-                            <SelectContent>
-                              <SelectItem value="true">Paid</SelectItem>
-                              <SelectItem value="false">Not Paid</SelectItem>
-                            </SelectContent>
-                          </Select>
+  
+                <div className="space-y-2">
+                  <div className="flex space-x-5">
+                    {/* view */}
+                    <Dialog >
+                      <DialogTrigger asChild>
+                        <Button disabled={order.status === 'Cancelled'} className="hover:bg-orange-100 rounded-none bg-transparent border-orange-400 hover:border-swiggyOrange border-2 text-swiggyOrange disabled:pointer-events-none">
+                          <Eye className="h-4 w-4" />View Order
+                        </Button>
+                      </DialogTrigger>
+                      <DialogContent>
+                        <DialogHeader>
+                          <DialogTitle>Order Details</DialogTitle>
+                        </DialogHeader>
+                        <OrderModal order={order} />
+                      </DialogContent>
+                    </Dialog>
+                    {/* Edit */}
+                    <Dialog open={dialogOpen === order._id} onOpenChange={(open) => {handleDialogChange(open, order)}}>
+                      <DialogTrigger asChild>
+                        <Button disabled={order.status === 'Cancelled'} className="flex-1 gap-2 rounded-none bg-orange-600 hover:bg-orange-700 disabled:pointer-events-none">
+                          <Edit2 className="w-4 h-4"/> Edit Status
+                        </Button>
+                      </DialogTrigger>
+                      <DialogContent>
+                        <DialogHeader>
+                          <DialogTitle>Update Order Status</DialogTitle>
+                        </DialogHeader>
+                        <div className="py-4">
+                          <div className="grid grid-cols-1 md:grid-cols-2">
+                            <Select value={order.status} onValueChange={(value) => handleStatusUpdate(order, value)}>
+                              <SelectTrigger className="w-full">
+                                <SelectValue placeholder="Select status" />
+                              </SelectTrigger>
+                              <SelectContent>
+                                <SelectItem value="Ordered">Ordered</SelectItem>
+                                <SelectItem value="Preparing">Preparing</SelectItem>
+                                {/* <SelectItem value="Completed">Completed</SelectItem> */}
+                                <SelectItem value="Delivered">Delivered</SelectItem>
+                                <SelectItem value="Cancelled">Cancelled</SelectItem>
+                              </SelectContent>
+                            </Select>
+                            <Select value={order.isPaid.toString()} onValueChange={(value) => handlePaymentStatus(order, value === "true")}>
+                              <SelectTrigger className="w-full">
+                                <SelectValue placeholder="Select status" />
+                              </SelectTrigger>
+                              <SelectContent>
+                                <SelectItem value="true">Paid</SelectItem>
+                                <SelectItem value="false">Not Paid</SelectItem>
+                              </SelectContent>
+                            </Select>
+                          </div>
                         </div>
-                      </div>
-                    </DialogContent>
-                  </Dialog>
+                      </DialogContent>
+                    </Dialog>
+                  </div>
                 </div>
               </div>
-            </div>
-          </CardContent>
-          <CardFooter className="bg-orange-50">
-            <div className="w-full mt-4">
-             <p className="h-8">
-                {order.remarks && (
-                  <p className="text-sm text-gray-600 italic">Note: {order.remarks.slice(-35)} <span>{order.remarks.length > 35 && "..."}</span></p>
-                )}
-             </p>
-              <div className="flex justify-between items-center mt-2">
-                <span className="text-sm font-medium">Total</span>
-                <span className="text-lg font-semibold">₹{order.totalAmount}</span>
+            </CardContent>
+            <CardFooter className="bg-orange-50">
+              <div className="w-full mt-4">
+               <p className="h-8">
+                  {order.remarks && (
+                    <p className="text-sm text-gray-600 italic">Note: {order.remarks.slice(-35)} <span>{order.remarks.length > 35 && "..."}</span></p>
+                  )}
+               </p>
+                <div className="flex justify-between items-center mt-2">
+                  <span className="text-sm font-medium">Total</span>
+                  <span className="text-lg font-semibold">₹{order.totalAmount}</span>
+                </div>
               </div>
-            </div>
-          </CardFooter>
-        </Card>
-      ))}
-      <Toaster />
-    </div>
+            </CardFooter>
+          </Card>
+        ))}
+        <Toaster />
+      </div>
+     )}
+    </>
   )
 }
 
