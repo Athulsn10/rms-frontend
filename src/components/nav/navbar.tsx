f53c9e9aea869b565c9d5b545b789c0c24c60d9f
Athulsn10 <athulnair10@outlook.com>
2025-02-16 21:39:09 +0530

    feat: display user location in the navbar and clean up location handling

--- src/components/nav/navbar.tsx
+++ src/components/nav/navbar.tsx
@@ -15,6 +15,7 @@ const Navbar = () => {
 
     const userName = localStorage.getItem('user');
     const restuarant = localStorage.getItem('restuarant');
+    const location = localStorage.getItem('city');
 
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
@@ -25,39 +26,23 @@ const Navbar = () => {
         setIsMenuOpen(false);
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         if (restuarant && userName) {
             setIsRestuarant(true);
         }
-    },[]);
+    }, []);
 
     return (
         <>
-            <nav className="bg-stone-50 shadow-sm sticky top-0 overflow-hidden z-20">
+            <nav className="bg-stone-50 shadow-sm sticky top-0 pt-2 overflow-hidden z-20">
                 <div className="mx-0 md:px-16 py-3">
-                    <div className="flex items-center justify-between mx-3">
+                    <div className="flex items-center justify-between mx-5 md:mx-1">
                         <div className="flex items-center gap-4 md:gap-6">
                             <div onClick={() => pushToPath("")} className="flex-shrink-0 cursor-pointer">
                                 <BrandLogo />
                             </div>
                             <div className="hidden md:flex items-center">
-                                <Popover>
-                                    <PopoverTrigger className='md:flex items-center gap-1 cursor-pointer hover:text-swiggyOrange'>
-                                        <span>Location</span>
-                                        <ChevronDown className="w-4 h-4" />
-                                    </PopoverTrigger>
-                                    <PopoverContent className='ms-16 mt-10 w-96'>
-                                        {/* search Bar */}
-                                        <div className='relative'>
-                                            <MapPin className='absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-gray-500' />
-                                            <Input className='p-8 pt-0 pb-0 pr-0 font-medium' placeholder='Search Location' />
-                                        </div>
-                                        {/* Search Result */}
-                                        <div className='h-64 flex items-center justify-center'>
-                                            <p>Nothing yet.. under dev..</p>
-                                        </div>
-                                    </PopoverContent>
-                                </Popover>
+                                <p className='flex items-center gap-2'><MapPin className="w-5 h-5" />{location}</p>
                             </div>
                         </div>
                         <div className="hidden md:flex items-center gap-8">
@@ -119,23 +104,6 @@ const Navbar = () => {
                                     <span>Scan Qr</span>
                                 </div>
                             )}
-                            <Popover>
-                                <PopoverTrigger className='flex items-center gap-2 cursor-pointer hover:text-swiggyOrange'>
-                                    <MapPin className="w-4 h-4" />
-                                    <span>Location</span>
-                                </PopoverTrigger>
-                                <PopoverContent className='mx-9 w-80'>
-                                    {/* search Bar */}
-                                    <div className='relative'>
-                                        <MapPin className='absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-gray-500' />
-                                        <Input className='p-8 pt-0 pb-0 pr-0 font-medium' placeholder='Search Location' />
-                                    </div>
-                                    {/* Search Result */}
-                                    <div className='h-64 flex items-center justify-center'>
-                                        <p>Nothing yet.. under dev..</p>
-                                    </div>
-                                </PopoverContent>
-                            </Popover>
                             <div onClick={() => pushToPath("search")} className="flex items-center gap-2 cursor-pointer hover:text-swiggyOrange">
                                 <Search className="w-5 h-5" />
                                 <span>Search</span>
@@ -146,7 +114,7 @@ const Navbar = () => {
                             </div>
                             {userName ? (
                                 <>
-                                    <div className='fixed bottom-0 left-0 m-4 pb-3'>
+                                    <div className='fixed bottom-14 left-0 m-4 pb-3'>
                                         <div onClick={() => pushToPath("profile")} className='flex items-center gap-2 cursor-pointer'>
                                             <Avatar>
                                                 <AvatarImage src="https://github.com/shadcn.png" />
